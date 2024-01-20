e95a308cf949cf6a233c7b045c2baa1f0d3a2c17
amit96pal <amit96pal.ap@gmail.com>
2024-01-20 22:05:07 +0530

    unary rpc

---
+++ src/main/java/com/vinsguru/server/Sec03gRPCIntro/BankService.java
@@ -0,0 +1,18 @@
+package com.vinsguru.server.Sec03gRPCIntro;
+
+import com.vinsguru.models.Balance;
+import com.vinsguru.models.BalanceCheckRequest;
+import com.vinsguru.models.BankServiceGrpc;
+import io.grpc.stub.StreamObserver;
+
+public class BankService extends BankServiceGrpc.BankServiceImplBase{
+    @Override
+    public void getBalance(BalanceCheckRequest request, StreamObserver<Balance> responseObserver) {
+        int accountNumber = request.getAccountNumber();
+        Balance balance = Balance.newBuilder()
+                .setAmount(AccountDatabase.getBalance(accountNumber))
+                .build();
+        responseObserver.onNext(balance);
+        responseObserver.onCompleted();
+    }
+}
---
+++ src/test/java/com/vinsguru/client/BankClientTest.java
@@ -0,0 +1,36 @@
+package com.vinsguru.client;
+
+import com.vinsguru.models.Balance;
+import com.vinsguru.models.BalanceCheckRequest;
+import com.vinsguru.models.BankServiceGrpc;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class BankClientTest {
+
+    private BankServiceGrpc.BankServiceBlockingStub blockingStub;
+
+    @BeforeAll
+    public void setUp() {
+        ManagedChannel managedChannel = ManagedChannelBuilder.forAddress("localhost", 6565)
+                .usePlaintext()
+                .build();
+
+        this.blockingStub = BankServiceGrpc.newBlockingStub(managedChannel);
+    }
+
+    @Test
+    void balanceTest() {
+        BalanceCheckRequest checkRequest = BalanceCheckRequest.newBuilder()
+                .setAccountNumber(2)
+                .build();
+
+        Balance balance = this.blockingStub.getBalance(checkRequest);
+
+        System.out.println("balance is "+balance.getAmount());
+    }
+}
